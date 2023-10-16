4194c4d1595cdfc882b3c533e6260005fd8b522e
João Vitor Machado <jozucjozuc@gmail.com>
2023-10-16 18:03:40 -0300

    G

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const containerCitacao = document.getElementById('container-citacao');
 const textoCitacao = document.getElementById('texto-citacao');
 const botaoObterCitacao = document.getElementById('botao-obter-citacao');
+const botaoCompartilharTwitter = document.getElementById('botao-compartilhar-twitter');
 
 async function buscarCitacaoKanye() {
     try {
@@ -8,6 +9,19 @@ async function buscarCitacaoKanye() {
         const data = await response.json();
         const citacao = data.quote;
         textoCitacao.textContent = `"${citacao}"`;
+
+
+        const tweetText = encodeURIComponent(`"${citacao}" - Ye`);
+        const tweetURL = " "; 
+
+
+        const twitterShareURL = `https://twitter.com/intent/tweet?text=${tweetText}&url=${tweetURL}`;
+
+
+        botaoCompartilharTwitter.setAttribute("href", twitterShareURL);
+
+
+        botaoCompartilharTwitter.style.display = "block";
     } catch (error) {
         console.error('Ocorreu um erro ao buscar a citação:', error);
         textoCitacao.textContent = 'Ocorreu um erro ao buscar a citação.';
